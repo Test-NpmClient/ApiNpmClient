1c54afa15801c6f90fcc6638671424fd0ceaf8ae
ishandry <andriy.ishchukk@gmail.com>
2024-08-13 14:46:29 +0300

    comment inti db

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -11,7 +11,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    await app.InitialiseDatabaseAsync();
+    //await app.InitialiseDatabaseAsync();
 }
 else
 {
