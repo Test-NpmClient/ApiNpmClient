76a50fcead3b62b8b73e064b3788c929e9cf2f23
ishandry <andriy.ishchukk@gmail.com>
2024-08-13 23:24:44 +0300

    applye api client

--- client/API/index.js
+++ client/API/index.js
@@ -3,6 +3,12 @@ const sayHello = function(name)
   console.log(`Hello, ORGGGanization!! ${name}`);
 }
 
+const sayBye = function(name)
+{
+  console.log(`Bye, ORGGGanization!! ${name}`);
+}
+
 module.exports = {
-    sayHello
+    sayHello,
+    sayBye
 }
